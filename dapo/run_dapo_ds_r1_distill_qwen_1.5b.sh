50e8e5c6dc589e28153284606f613c0a49a84f6a
Daniel Shen <dshen@crusoeenergy.com>
2025-09-19 22:26:10 -0700

    feat: run_dapo_ds_r1_distill_qwen_1.5b.sh starter

---
+++ dapo/run_dapo_ds_r1_distill_qwen_1.5b.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+set -xeuo pipefail
+
+project_name='DAPO'
+exp_name='DAPO-DS-R1-Distill-Qwen-1.5B'
+
+# Ray
+RAY_ADDRESS=${RAY_ADDRESS:-"http://localhost:8265"}
+WORKING_DIR=${WORKING_DIR:-"${PWD}"}
+RUNTIME_ENV=${RUNTIME_ENV:-"${WORKING_DIR}/dapo/runtime_env.yaml"}
+# Paths
+RAY_DATA_HOME=${RAY_DATA_HOME:-"${HOME}/nemotron-research-reasoning-qwen-1.5b"}
+TRAIN_FILE=${TRAIN_FILE:-"${RAY_DATA_HOME}/data/deepscaler/train.parquet"}
+
+
+ray job submit --no-wait --runtime-env="${RUNTIME_ENV}" \
+    --working-dir "${WORKING_DIR}" \
+    -- python3 -m verl.recipe.dapo.main_dapo \
+    data.train_files="${TRAIN_FILE}" \
+
