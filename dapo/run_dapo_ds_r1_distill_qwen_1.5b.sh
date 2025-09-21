3a7a86becf028c5515fafe9557d4b6e5f94c4434
Daniel Shen <dshen@crusoeenergy.com>
2025-09-20 17:16:42 -0700

    feat: data params

--- dapo/run_dapo_ds_r1_distill_qwen_1.5b.sh
+++ dapo/run_dapo_ds_r1_distill_qwen_1.5b.sh
@@ -6,6 +6,11 @@ PROJECT_DIR=${PROJECT_DIR:-"${PWD}/nemotron-research-reasoning-qwen-1.5b"}
 project_name='DAPO'
 exp_name='DAPO-DS-R1-Distill-Qwen-1.5B'
 
+# DeepScaleR prompts are usually < 100 tokens, so this is very comfortable
+max_prompt_length=1024
+# "Scaling Up RL" sets context window limit at 8096
+max_response_length=$((8096 - 1024))
+
 # Ray
 RAY_ADDRESS=${RAY_ADDRESS:-"http://localhost:8265"}
 WORKING_DIR=${WORKING_DIR:-"${PWD}"}
@@ -21,6 +26,9 @@ ray job submit --no-wait --runtime-env="${RUNTIME_ENV}" \
     -- python3 -m verl.recipe.dapo.main_dapo \
     data.train_files="${TRAIN_FILE}" \
     data.val_files="${TEST_FILE}" \
+    data.prompt_key=prompt \
+    data.max_prompt_length=${max_prompt_length} \
+    data.max_response_length=${max_response_length} \
     custom_reward_function.path=${PROJECT_DIR}/reward/rllm_reward.py \
     custom_reward_function.name=rllm_reward_fn_math_transformed \
 
