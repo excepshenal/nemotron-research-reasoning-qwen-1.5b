ff0703ecd1b414d6a338fc33b6bc37679e68f9f1
Daniel Shen <dshen@crusoeenergy.com>
2025-09-20 18:41:40 -0700

    fix: clean up env vars

--- dapo/run_dapo_ds_r1_distill_qwen_1.5b.sh
+++ dapo/run_dapo_ds_r1_distill_qwen_1.5b.sh
@@ -1,11 +1,6 @@
 #!/usr/bin/env bash
 set -xeuo pipefail
 
-PROJECT_DIR=${PROJECT_DIR:-"${PWD}/nemotron-research-reasoning-qwen-1.5b"}
-
-project_name='DAPO'
-exp_name='DAPO-DS-R1-Distill-Qwen-1.5B'
-
 # DeepScaleR prompts are usually < 100 tokens, so this is very comfortable
 max_prompt_length=1024
 # "Scaling Up RL" sets context window limit at 8096
@@ -17,17 +12,15 @@ train_prompt_mini_bsz=64
 # use dynamic micro batch size
 ppo_max_token_len_per_gpu=16384 # from verl example dapo scripts
 
-MODEL_PATH=${WORKING_DIR:-"~/models/ds-r1-distill-qwen-1.5b"}
+PROJECT_DIR=${PROJECT_DIR:-"${HOME}/nemotron-research-reasoning-qwen-1.5b"}
+MODEL_PATH=${MODEL_PATH:-"${HOME}/models/ds-r1-distill-qwen-1.5b"}
+TRAIN_FILE=${TRAIN_FILE:-"${HOME}/data/deepscaler/train.parquet"}
+TEST_FILE=${TEST_FILE:-"${HOME}/data/aime24/test.parquet"}
 
 # Ray
 RAY_ADDRESS=${RAY_ADDRESS:-"http://localhost:8265"}
-WORKING_DIR=${WORKING_DIR:-"${PWD}"}
-RUNTIME_ENV=${RUNTIME_ENV:-"${WORKING_DIR}/dapo/runtime_env.yaml"}
-# Paths
-RAY_DATA_HOME=${RAY_DATA_HOME:-"${HOME}/nemotron-research-reasoning-qwen-1.5b"}
-TRAIN_FILE=${TRAIN_FILE:-"${RAY_DATA_HOME}/data/deepscaler/train.parquet"}
-TEST_FILE=${TEST_FILE:-"${RAY_DATA_HOME}/data/aime24/test.parquet"}
-
+WORKING_DIR=${WORKING_DIR:-"${PROJECT_DIR}"}
+RUNTIME_ENV=${RUNTIME_ENV:-"${PROJECT_DIR}/dapo/runtime_env.yaml"}
 
 ray job submit --no-wait --runtime-env="${RUNTIME_ENV}" \
     --working-dir "${WORKING_DIR}" \
@@ -44,4 +37,3 @@ ray job submit --no-wait --runtime-env="${RUNTIME_ENV}" \
     actor_rollout_ref.actor.ppo_max_token_len_per_gpu=${ppo_max_token_len_per_gpu} \
     custom_reward_function.path=${PROJECT_DIR}/reward/rllm_reward.py \
     custom_reward_function.name=rllm_reward_fn_math_transformed \
-
