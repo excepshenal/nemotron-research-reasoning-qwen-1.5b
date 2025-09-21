43ae71d304503b74654a771c90a5c4d07a25fe78
Daniel Shen <dshen@crusoeenergy.com>
2025-09-21 14:09:13 -0700

    feat: actor_rollout_ref.actor dapo training params

--- dapo/run_dapo_ds_r1_distill_qwen_1.5b.sh
+++ dapo/run_dapo_ds_r1_distill_qwen_1.5b.sh
@@ -1,16 +1,31 @@
 #!/usr/bin/env bash
 set -xeuo pipefail
 
+# Params from "Scaling Up RL" paper unless otherwise specified
+
+# Data
+
 # DeepScaleR prompts are usually < 100 tokens, so this is very comfortable
 max_prompt_length=1024
 # "Scaling Up RL" sets context window limit at 8096
 max_response_length=$((8096 - 1024))
 
-# Batch size params from "Scaling Up RL" paper
+# Actor
+
 train_prompt_bsz=256
 train_prompt_mini_bsz=64
 # use dynamic micro batch size
 ppo_max_token_len_per_gpu=16384 # from verl example dapo scripts
+
+clip_ratio_low=0.2
+clip_ratio_high=0.4
+
+use_kl_loss=True
+kl_loss_coef=0.0001
+
+actor_optim_lr=2e-6
+
+# Filepaths
 
 PROJECT_DIR=${PROJECT_DIR:-"${HOME}/nemotron-research-reasoning-qwen-1.5b"}
 MODEL_PATH=${MODEL_PATH:-"${HOME}/models/ds-r1-distill-qwen-1.5b"}
@@ -22,6 +37,11 @@ RAY_ADDRESS=${RAY_ADDRESS:-"http://localhost:8265"}
 WORKING_DIR=${WORKING_DIR:-"${PROJECT_DIR}"}
 RUNTIME_ENV=${RUNTIME_ENV:-"${PROJECT_DIR}/dapo/runtime_env.yaml"}
 
+# Defaults are set in verl/recipe/dapo/config/dapo_trainer.yaml,
+# which itself references verl/trainer/config/ppo_trainer.yaml.
+# Some defaults include:
+# - actor_rollout_ref.actor.strategy: fsdp
+# - fsdp offload params: False (unnecessary for 1.5b model)
 ray job submit --no-wait --runtime-env="${RUNTIME_ENV}" \
     --working-dir "${WORKING_DIR}" \
     -- python3 -m verl.recipe.dapo.main_dapo \
@@ -37,5 +57,11 @@ ray job submit --no-wait --runtime-env="${RUNTIME_ENV}" \
     actor_rollout_ref.actor.ppo_mini_batch_size=${train_prompt_mini_bsz} \
     actor_rollout_ref.actor.use_dynamic_bsz=True \
     actor_rollout_ref.actor.ppo_max_token_len_per_gpu=${ppo_max_token_len_per_gpu} \
+    actor_rollout_ref.actor.clip_ratio_low=${clip_ratio_low} \
+    actor_rollout_ref.actor.clip_ratio_high=${clip_ratio_high} \
+    actor_rollout_ref.actor.use_kl_loss=${use_kl_loss} \
+    actor_rollout_ref.actor.kl_loss_coef=${kl_loss_coef} \
+    actor_rollout_ref.actor.use_torch_compile=True \
+    actor_rollout_ref.actor.optim.lr=${actor_optim_lr} \
     custom_reward_function.path=${PROJECT_DIR}/reward/rllm_reward.py \
     custom_reward_function.name=rllm_reward_fn_math_transformed \
